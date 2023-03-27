dc4b7eecb3fbbffb95c0baa8484dbbbc99ed6368
WIN ENG <WIN ENG@GE121-PC15>
2023-03-27 04:07:46 -0700

    Add project files.

---
+++ ConsoleApp3/BasicMaths.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class BasicMaths
+    {
+        static void Main(string[] args)
+        {
+        }
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
